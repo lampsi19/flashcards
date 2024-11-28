ae7f1a952cac4dae29125fa0b250da72d7b84736
lampsi19 <p.ana19@escola.pr.gov.br>
2024-11-28 18:59:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+   'Programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'O maior deserto do mundo é o Deserto do Saara'
+)
+
+criaCartao(
+    'Cultura geral',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'A fórmula química da água é H₂O'
+)
